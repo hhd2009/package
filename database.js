55057951f33e9c28e809eec05ccca5e387036f61
hdlr <95166106+hhd2009@users.noreply.github.com>
2022-11-24 09:52:05 +0800

    Update database.js

--- database.js
+++ database.js
@@ -1,24 +1,21 @@
 var fso = new ActiveXObject("Scripting.FileSystemObject")
 
-// 创建文件
-function create_file(name){
+// 创建
+function create_db(name){
   fso.CreateTextFile(name, true)
 }
 
-// 填写一行数据
-function write_file(name,msg){
+// 填写
+function write_db(name,msg){
   var f=fso.OpenTextFile(name,1,true)
   f.Write(msg)
   f.Close()
 }
-f1.WriteBlankLines(1)
-// 关闭文件
-f1.Close()
-// 打开文件
-ts = fso.OpenTextFile("c:\\testfile.txt", ForReading)
-// 读取文件一行内容到字符串
-s = ts.ReadLine()
-// 显示字符串信息
-alert("File contents = '" + s + "'")
-// 关闭文件
-ts.Close()
+
+// 读取
+function read_db(name){
+  var f=fso.OpenTextFile(name, 1)
+  msg=f.ReadLine()
+  f.Close()
+  return msg
+}
